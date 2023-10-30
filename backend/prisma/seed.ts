c35f40c1d85ecf1a85036142fef0cbd302d2c8eb
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-10-30 15:24:10 -0600

    Modified seed file

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,20 +1,38 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import { Prettify } from "elysia/dist/types";
 
-await prisma.role.createMany({
-  data: [{ roleName: "admin" }, { roleName: "user" }, { roleName: "premium" }],
-});
+async function run() {
+  console.log("Running seed script...");
+  console.time();
 
-const user = await prisma.user.create({
-  data: {
-    firstName: "Daniel",
-    secondName: "Sebastian",
-    lastName: "Cajas",
-    birthDate: new Date("11/21/2003"),
-    sex: "MALE",
-    roles: {
-      connect: {roleName: "admin"},
-    },
-  },
-});
+  try {
+    await prisma.role.createMany({
+      data: [
+        { roleName: "admin" },
+        { roleName: "user" },
+        { roleName: "premium" },
+      ],
+    });
+
+    await prisma.user.create({
+      data: {
+        firstName: "Daniel",
+        secondName: "Sebastian",
+        lastName: "Cajas",
+        birthDate: new Date("11/21/2003"),
+        sex: "MALE",
+        roles: {
+          connect: { roleName: "admin" },
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Error running the seed script: ");
+    console.error(error);
+  } finally {
+    console.log("Finished running seed script");
+    console.timeEnd();
+  }
+}
+
+await run();
