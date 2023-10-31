300320fe8c9fe73f56785c38023aa196f48dc7e5
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-10-30 21:50:32 -0600

    Added support to post new routine templates

--- backend/src/routes/routines.ts
+++ backend/src/routes/routines.ts
@@ -1,8 +1,27 @@
-import { Elysia } from "elysia";
+import { Elysia, t, Static } from "elysia";
 import { RoutineService } from "../services/routine.service";
 
+const RoutineTemplateSchema = t.Object({
+  title: t.String(),
+  description: t.Optional(t.String()),
+  userId: t.String(),
+  tags: t.Optional(t.Array(t.String())),
+});
+
+export type RoutineTemplateType = Static<typeof RoutineTemplateSchema>;
+
 export const routineApp = new Elysia()
-  .get("/templates", async () => await RoutineService.getRoutineTemplates())
+  .group("/templates", (app) =>
+    app
+      .get("/", async () => await RoutineService.getRoutineTemplates())
+      .post(
+        "/",
+        async ({ body }) => await RoutineService.postRoutineTemplate(body),
+        {
+          body: RoutineTemplateSchema,
+        }
+      )
+  )
   .get("/executions/:templateId", async ({ params, set }) => {
     const routine = await RoutineService.getRoutineWithExecutions(
       params.templateId
--- backend/src/services/routine.service.ts
+++ backend/src/services/routine.service.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../client";
+import { RoutineTemplateType } from "../routes/routines";
 
 export class RoutineService {
   static async getRoutineTemplates() {
@@ -9,6 +10,17 @@ export class RoutineService {
     return await prisma.routineTemplate.findUnique({
       where: { routineTemplateId: id },
       include: { routineExecutions: true },
+    });
+  }
+
+  static async postRoutineTemplate(data: RoutineTemplateType) {
+    return await prisma.routineTemplate.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        userIdFK: data.userId,
+        tags: { connect: data.tags?.map((tag) => ({ name: tag })) },
+      },
     });
   }
 }
