941a60f3b05be5909b2ad215f2e216fcc12c52ca
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-11-11 18:01:27 -0600

    Add CORS middleware and update dependencies.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { routineApp } from "./routes/routines";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+  .use(cors())
   .group("/routines", (app) => app.use(routineApp))
   .listen(process.env.PORT || 3000);
 
